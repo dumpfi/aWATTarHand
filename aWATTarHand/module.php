5184c8fe8a0499ab6176bae22b855633c9a3276e
dumpfi <124400975+dumpfi@users.noreply.github.com>
2023-02-04 20:51:06 +0100

    V0.1.1
    Korrektur

--- aWATTarHand/module.php
+++ aWATTarHand/module.php
@@ -2,78 +2,87 @@
 
     //Klassendefinition
     class aWATTarHand extends IPSModule {
-        // Diese Zeile nicht löschen.
-        parent::Create();
 
-        $this->RegisterVariableInteger("datetoday", "Datum Heute");
-        $this->RegisterVariableInteger("datetomorow", "Datum Morgen");
-    }
+        public function Create(){
+            
+            // Diese Zeile nicht löschen.
+            parent::Create();
 
-    // Überschreibt die intere IPS_ApplyChanges($id) Funktion
-    public function ApplyChanges() {
-        // Diese Zeile nicht löschen
-        parent::ApplyChanges();
-    }
-
-    /**
-    * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
-    * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
-    *
-    * ABC_MeineErsteEigeneFunktion($id);
-    *
-    */
-
-    public function MeineErsteEigeneFunktion() {
-        // Selbsterstellter Code
-
-        //API Abfrage aWATTar
-
-        //URL Für aWATTar Schnitstellenabfrage aufbereiten
-        $ch = curl_init();
-
-        $urlpart1 = 'https://api.awattar.at/v1/marketdata?start=';
-        $urlpart2 = strtotime(date('d.m.Y 00:00:00')) * 1000;
-        $urlpart3 = '&end=';
-
-        $timestamp1 = time();
-        $merkertime1 = (strtotime(date('d.m.Y 00:00:00')) + 46800);
-
-        if ($timestamp1 >= $merkertime1) {
-
-            $urlpart4 = (strtotime(date('d.m.Y 00:00:00')) + 172800) * 1000;
-
-        } else {
-
-            $urlpart4 = (strtotime(date('d.m.Y 00:00:00')) + 86400) * 1000;
+            $this->RegisterVariableInteger("datetoday", "Datum Heute");
+            $this->RegisterVariableInteger("datetomorow", "Datum Morgen");
 
         }
 
-        //URL Bilden
-        $url = $urlpart1 . $urlpart2 . $urlpart3 . $urlpart4;
 
-        //API Abfrage iniziieren
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        //DAten abfragen
-        $resp = curl_exec($ch);
-
-        //if Abfrage für Error
-        if($e = curl_error($ch)){
-            echo $e;
-        } else{
-            $decoded = json_decode($resp, true);
-            //print_r($decoded);
-            //echo "Funktioniert <br><br>";
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
         }
 
-        //Api Verbindung schließen
-        curl_close($ch);
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
 
-        //echo 'geht' . "\n";
-        //echo $url . "\n";
-        //echo $decoded;
+
+        public function MeineErsteEigeneFunktion() {
+            // Selbsterstellter Code
+    
+            //API Abfrage aWATTar
+    
+            //URL Für aWATTar Schnitstellenabfrage aufbereiten
+            $ch = curl_init();
+    
+            $urlpart1 = 'https://api.awattar.at/v1/marketdata?start=';
+            $urlpart2 = strtotime(date('d.m.Y 00:00:00')) * 1000;
+            $urlpart3 = '&end=';
+    
+            $timestamp1 = time();
+            $merkertime1 = (strtotime(date('d.m.Y 00:00:00')) + 46800);
+    
+            if ($timestamp1 >= $merkertime1) {
+    
+                $urlpart4 = (strtotime(date('d.m.Y 00:00:00')) + 172800) * 1000;
+    
+            } else {
+    
+                $urlpart4 = (strtotime(date('d.m.Y 00:00:00')) + 86400) * 1000;
+    
+            }
+    
+            //URL Bilden
+            $url = $urlpart1 . $urlpart2 . $urlpart3 . $urlpart4;
+    
+            //API Abfrage iniziieren
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+            //DAten abfragen
+            $resp = curl_exec($ch);
+    
+            //if Abfrage für Error
+            if($e = curl_error($ch)){
+                echo $e;
+            } else{
+                $decoded = json_decode($resp, true);
+                //print_r($decoded);
+                //echo "Funktioniert <br><br>";
+            }
+    
+            //Api Verbindung schließen
+            curl_close($ch);
+    
+            //echo 'geht' . "\n";
+            //echo $url . "\n";
+            //echo $decoded;
+        }
+        
     }
+  
 
 
 
